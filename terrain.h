d1db92fb148a5fdefff98cc5cd2d0a177b7dacf4
Kou Kigaku <211112@st.yoshida-g.ac.jp>
2021-12-10 14:09:49 +0900

    bkkbkbkbkkb

---
+++ terrain.h
@@ -0,0 +1,17 @@
+/*
+--------------------------------------------------
+Author: Huang QiYue
+--------------------------------------------------
+*/
+#ifndef _Terrain_H_
+#define _Terrain_H_
+
+#include "main.h"
+
+void InitTerrain(void);		// Terrain‚Ì‰Šú‰»ˆ—
+void UninitTerrain(void);	// Terrain‚ÌI—¹ˆ—
+void UpdateTerrain(void);	// Terrain‚ÌXVˆ—
+void DrawTerrain(void);		// Terrain‚Ì•`‰æˆ—
+
+
+#endif // !_Terrain_H_
